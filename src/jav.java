efb2f1e85fcb2b12d0716dcd9985e27082e76131
bereket <motta4259@gmail.com>
2019-06-12 10:29:40 -0400

    first commit

---
+++ src/jav.java
@@ -0,0 +1,10 @@
+public class jav {
+
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+
+        System.out.println("Today is July 12, 2019. I am alive!");
+        System.out.println("My name is Bereket");
+    }
+}
+
